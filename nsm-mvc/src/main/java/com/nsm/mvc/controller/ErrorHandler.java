a8dc5e879b4224141a29a7d8a42728f92303783a
nieshuming <nieshuming@gcall.com>
2018-05-29 23:24:10 +0800

    modify logger

--- nsm-mvc/src/main/java/com/nsm/mvc/controller/ErrorHandler.java
+++ nsm-mvc/src/main/java/com/nsm/mvc/controller/ErrorHandler.java
@@ -2,8 +2,7 @@ package com.nsm.mvc.controller;
 
 import com.nsm.mvc.exception.BusinessException;
 import com.nsm.mvc.exception.ErrorCode;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Created by Administrator on 2018/5/27.
  */
 public class ErrorHandler {
-    private Log logger = LogFactory.getLog(ErrorHandler.class);
+    private Logger logger = Logger.getLogger(ErrorHandler.class);
     /**
      * 全局异常捕捉处理
      * @param ex
